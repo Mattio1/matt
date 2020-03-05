393b09e4ef7b1b72739461b7825eedcb3164d6e4
Mateusz Aleksander Kucharczyk <mk417796@pd037.odk.fuw.edu.pl>
2020-03-05 13:37:27 +0100

    Druga wersja funkcji zmierz(), dodana silnia iteracyjna

--- szybkosc.py
+++ szybkosc.py
@@ -1,17 +1,23 @@
 import time
 
 def zmierz(f, n=100):
-    """Funkcja mierzy czas wykonania funkcji podanejjako argument dla podanego argumentu"""
+    """Funkcja mierzy czas wykonania funkcji podanej jako argument dla podanego argumentu"""
     t0=time.time()
     f(n)
     t1=time.time()
     return t1 - t0
 
-def silnia(n):
+def silnia_rek(n):
     if n>1:
-        return silnia(n-1)*n
+        return silnia_rek(n-1)*n
     else:
         return 1
     
+def silnia_it(n):
+    a=1
+    for n in range(1,n-1):
+        a = a*n
+    return a
 
-print("Silnia rekurencyjnie: ", zmierz(silnia))
+print("Silnia rekurencyjnie: ", zmierz(silnia_rek))
+print("Silnia iteracyjnie: ", zmierz(silnia_it))
